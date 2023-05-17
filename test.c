1218d93f71b15c1e6724577c9430fb5d152b6950
fndl5759 <62679443+fndl5759@users.noreply.github.com>
2023-05-17 11:25:31 +0900

    menu.c 구현
    메인 메뉴와
    서브메뉴간 이동을 구현했습니다.
    기본적인 메뉴 제작도 함수로 바꿨고
    중복부분은 좀 더 다듬어 함수로 옮길예정입니다.
    포멧은 아마 이대로 쭉 갈것같습니다.

--- test.c
+++ test.c
@@ -20,6 +20,15 @@ char *Ichoices[] = {
     "Save & Exit",
 };
 
+MENU *create_menu(char **choices, int n_choices) {
+    ITEM **my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
+    
+    for (int i = 0; i < n_choices; ++i)
+        my_items[i] = new_item(choices[i], "");
+    MENU *my_menu = new_menu((ITEM **)my_items);
+    return my_menu;
+}
+
 int main()
 {
     initscr();
@@ -31,24 +40,17 @@ int main()
     init_pair(2, COLOR_BLUE, COLOR_WHITE);
 
 //메뉴 구성 및 생성
-    int n_choices = sizeof(choices) / sizeof(char *);
-    ITEM **my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
-    for (int i = 0; i < n_choices; ++i)
-        my_items[i] = new_item(choices[i], "");
-        
-    MENU *my_menu = new_menu((ITEM **)my_items);
-    
-//Input Flags 메뉴
-    int i_choices = sizeof(Ichoices) / sizeof(char *);
-    ITEM **i_items = (ITEM **)calloc(i_choices + 1, sizeof(ITEM *));
-    for (int i = 0; i < i_choices; ++i)
-        i_items[i] = new_item(Ichoices[i], "");
-        
-    MENU *i_menu = new_menu((ITEM **)i_items);
+    int n_choices,i_choices,o_choices,l_choices;
+    n_choices = sizeof(choices)/ sizeof(char*);
+    i_choices = sizeof(Ichoices)/ sizeof(char*);
+    MENU *my_menu = create_menu(choices, n_choices);
+    MENU *i_menu = create_menu(Ichoices, i_choices);
+    //MENU *o_menu = create_menu(Ochoices, o_choices);
+    //MENU *l_menu = create_menu(Lchoices, l_choices);
 
     int max_y, max_x;
     getmaxyx(stdscr, max_y, max_x); 
-    WINDOW *menu_win = newwin(max_y-2, max_x-2, 1, 1); 
+    WINDOW *menu_win = newwin(max_y-4, max_x-4, 1, 1); 
     WINDOW *sub_win = derwin(menu_win, 8, 20, max_y/2-6, max_x/2-9); 
  //메뉴 출력    
     keypad(menu_win, TRUE);
@@ -60,79 +62,85 @@ int main()
     set_menu_fore(my_menu, COLOR_PAIR(2));
     set_menu_back(my_menu, COLOR_PAIR(1));
     box(menu_win, 0, 0);
-    mvwprintw(menu_win, 2, max_x/2 - 8, "TERMINAL SETTING");
-    mvwprintw(menu_win, max_y-4, max_x - 14, "F1 to Exit");
-    post_menu(my_menu);
-    wrefresh(menu_win);
+    mvwprintw(menu_win, 0, max_x/2 - 10, "TERMINAL SETTING");
+    mvwprintw(menu_win, max_y-5, max_x - 15, "F1 to Exit");
+    
+    WINDOW *iflags_win = newwin(max_y-4, max_x-4, 1, 1);
+    WINDOW *isub_win = derwin(iflags_win, 8, 20, 5, 13); 
+    keypad(iflags_win, TRUE);
+    set_menu_win(i_menu, iflags_win);
+    set_menu_sub(i_menu, isub_win);
+           	
+    wbkgd(stdscr, COLOR_PAIR(1));
+    wbkgd(iflags_win, COLOR_PAIR(1));
+    set_menu_fore(i_menu, COLOR_PAIR(2));   
+    set_menu_back(i_menu, COLOR_PAIR(1));
+    box(iflags_win,0,0);
+    mvwprintw(iflags_win, 0, max_x/2 - 10, "TERMINAL SETTING");
+    mvwprintw(iflags_win, 2, max_x/2 - 8, "Input  Flags");
+    mvwprintw(iflags_win, 5, max_x - 18, "ON");
+    mvwprintw(iflags_win, 6, max_x - 18, "ON");
+    mvwprintw(iflags_win, 7, max_x - 18, "ON");
+    mvwprintw(iflags_win, 8, max_x - 18, "ON");
+    mvwprintw(iflags_win, 9, max_x - 18, "ON");
+    mvwprintw(iflags_win, 10, max_x - 18, "ON");
+    mvwprintw(iflags_win, 11, max_x - 18, "ON");
+            	
+ 
  
 //사용자 입력 처리    
     int c;
+    int m=1,i=0,o=0,l=0;
     ITEM *cur_item;
-    while ((c = wgetch(menu_win)) != KEY_F(1))
-        switch (c)
-        {
-        case KEY_DOWN:
-            menu_driver(my_menu, REQ_DOWN_ITEM);
-            break;
-        case KEY_UP:
-            menu_driver(my_menu, REQ_UP_ITEM);
-            break;
-        case 10: // Enter로 선택
-            cur_item = current_item(my_menu);
-            if (strcmp(item_name(cur_item), "Input  Flags") == 0) {
-            	
-            	WINDOW *input_flags_win = newwin(max_y-2, max_x-2, 1, 1);
-            	WINDOW *isub_win = derwin(input_flags_win, 8, 20, 5, 13); 
-    
-            	keypad(input_flags_win, TRUE);
-            	set_menu_win(i_menu, input_flags_win);
-            	set_menu_sub(i_menu, isub_win);
-            	
-            	wbkgd(stdscr, COLOR_PAIR(1));
-            	wbkgd(input_flags_win, COLOR_PAIR(1));
-		set_menu_fore(i_menu, COLOR_PAIR(2));   
-		set_menu_back(i_menu, COLOR_PAIR(1));
-            	box(input_flags_win,0,0);
-            	mvwprintw(input_flags_win, 2, max_x/2 - 8, "Input  Flags");
-    		mvwprintw(input_flags_win, 5, max_x - 18, "ON");
-            	mvwprintw(input_flags_win, 6, max_x - 18, "ON");
-            	mvwprintw(input_flags_win, 7, max_x - 18, "ON");
-            	mvwprintw(input_flags_win, 8, max_x - 18, "ON");
-            	mvwprintw(input_flags_win, 9, max_x - 18, "ON");
-            	mvwprintw(input_flags_win, 10, max_x - 18, "ON");
-            	mvwprintw(input_flags_win, 11, max_x - 18, "ON");
-            	
-            	post_menu(i_menu);
-            	wrefresh(input_flags_win);
-		while ((c = wgetch(input_flags_win)) != -1)
+    while (c != KEY_F(1)){
+    	if(m==1){
+	    	post_menu(my_menu);
+	    	touchwin(menu_win);
+		wrefresh(menu_win);
+	    	c = wgetch(menu_win);
 		switch (c)
-        {
-        case KEY_DOWN:
-            menu_driver(i_menu, REQ_DOWN_ITEM);
-            break;
-        case KEY_UP:
-            menu_driver(i_menu, REQ_UP_ITEM);
-            break;
-        case 10:  
-        cur_item = current_item(i_menu);
-        if (strcmp(item_name(cur_item), "Exit") == 0) {
-        	unpost_menu(i_menu);
-        	wrefresh(input_flags_win);
-        	delwin(input_flags_win);
-        	touchwin(menu_win);
-        	wrefresh(menu_win);
-        	d = -1;
+		{
+			case KEY_DOWN:
+			    menu_driver(my_menu, REQ_DOWN_ITEM);
+			    break;
+			case KEY_UP:
+			    menu_driver(my_menu, REQ_UP_ITEM);
+			    break;
+			case 10: 
+			    cur_item = current_item(my_menu);
+			    if (strcmp(item_name(cur_item), "Input  Flags") == 0) {
+			    	m=0,i=1;
+			    	break;	     
+		    }
+		}
         }
-              
+        if(m==0 && i == 1){
+            	post_menu(i_menu);
+            	touchwin(iflags_win);
+            	wrefresh(iflags_win);
+            	c=wgetch(iflags_win);
+		switch (c)
+		{
+			case KEY_DOWN:
+			    menu_driver(i_menu, REQ_DOWN_ITEM);
+			    break;
+			case KEY_UP:
+			    menu_driver(i_menu, REQ_UP_ITEM);
+			    break;
+			case 10:  
+			cur_item = current_item(i_menu);
+			if (strcmp(item_name(cur_item), "Exit") == 0) {
+				m=1,i=0;
+				mvwprintw(menu_win, 16, max_x , "m = %d, i = %d",m,i);
+				break;
+			}
+		}
         }
-        }
-               
+        
     }
 //초기화
     unpost_menu(my_menu);
     free_menu(my_menu);
-    for (int i = 0; i < n_choices; ++i)
-        free_item(my_items[i]);
     endwin();
 }
   
