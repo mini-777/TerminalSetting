b831ab16c4c9dba6f27dac33567b526edef509ad
Min <min@Minui-MacBookPro.local>
2023-05-22 11:29:14 +0900

    update test2.c

--- test2.c
+++ test2.c
@@ -46,7 +46,7 @@ void toggle_echo()
 
 int main()
 {
-    ITEM *my_items[3];
+    ITEM *my_items[4];
     MENU *my_menu;
     WINDOW *my_menu_win;
     int i;
@@ -66,6 +66,7 @@ int main()
     my_items[0] = new_item("Toggle Echo", "");
     my_items[1] = new_item("Canonical Mode", "");
     my_items[2] = new_item("Non-canonical Mode", "");
+    my_items[3] = NULL; // Add NULL item to mark the end
 
     /* Create menu */
     my_menu = new_menu((ITEM **)my_items);
@@ -89,47 +90,47 @@ int main()
     refresh();
 
     /* Loop to capture user input */
-    // while ((c = wgetch(my_menu_win)) != KEY_F(1))
-    // {
-    //     switch (c)
-    //     {
-    //     case KEY_DOWN:
-    //         menu_driver(my_menu, REQ_DOWN_ITEM);
-    //         break;
-    //     case KEY_UP:
-    //         menu_driver(my_menu, REQ_UP_ITEM);
-    //         break;
-    //     case 10: /* Enter key */
-    //     {
-    //         ITEM *cur_item = current_item(my_menu);
-    //         const char *name = item_name(cur_item);
+    while ((c = wgetch(my_menu_win)) != KEY_F(1))
+    {
+        switch (c)
+        {
+        case KEY_DOWN:
+            menu_driver(my_menu, REQ_DOWN_ITEM);
+            break;
+        case KEY_UP:
+            menu_driver(my_menu, REQ_UP_ITEM);
+            break;
+        case 10: /* Enter key */
+        {
+            ITEM *cur_item = current_item(my_menu);
+            const char *item_name = item_name(cur_item);
 
-    //         if (strcmp(name, "Toggle Echo") == 0)
-    //         {
-    //             toggle_echo();
-    //         }
-    //         else if (strcmp(name, "Canonical Mode") == 0)
-    //         {
-    //             set_canonical_mode();
-    //         }
-    //         else if (strcmp(name, "Non-canonical Mode") == 0)
-    //         {
-    //             set_noncanonical_mode();
-    //         }
+            if (strcmp(item_name, "Toggle Echo") == 0)
+            {
+                toggle_echo();
+            }
+            else if (strcmp(item_name, "Canonical Mode") == 0)
+            {
+                set_canonical_mode();
+            }
+            else if (strcmp(item_name, "Non-canonical Mode") == 0)
+            {
+                set_noncanonical_mode();
+            }
 
-    //         pos_menu_cursor(my_menu);
-    //         break;
-    //     }
-    //     }
+            pos_menu_cursor(my_menu);
+            break;
+        }
+        }
 
-    //     wrefresh(my_menu_win);
-    // }
+        wrefresh(my_menu_win);
+    }
 
-    // /* Free menu items */
-    // for (i = 0; i < ARRAY_SIZE(my_items); ++i)
-    // {
-    //     free_item(my_items[i]);
-    // }
+    /* Free menu items */
+    for (i = 0; i < ARRAY_SIZE(my_items) - 1; ++i)
+    {
+        free_item(my_items[i]);
+    }
 
     /* Free menu */
     free_menu(my_menu);
