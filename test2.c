3540263097c42678c751d59ef5b1969e6515f8e4
mini-777 <pho0570@gmail.com>
2023-05-23 16:23:44 +0900

    [update] test2.c c_iflag IGNBRK toggle 기능 추가

--- test2.c
+++ test2.c
@@ -7,5 +7,14 @@ int main()
     struct termios old_settings;
     tcgetattr(0, &old_settings);
     printf("%d", old_settings.c_iflag & IGNBRK);
+    if (old_settings.c_iflag & IGNBRK)
+    {
+        old_settings.c_iflag &= ~IGNBRK;
+    }
+    else
+    {
+        old_settings.c_iflag |= IGNBRK;
+    }
+    tcsetattr(0, TCSANOW, &old_settings);
     return 0;
 }
